8e5fdfd4462fd61ead0132409b4fe49c08c27485
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-27 22:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    let currentCircle = 1;
+
+    function updateButtons() {
+        prevButton.disabled = currentCircle === 1;
+        nextButton.disabled = currentCircle === circles.length;
+    }
+
+    function updateProgress() {
+        circles.forEach((circle, index) => {
+            circle.classList.toggle('active', index + 1 <= currentCircle);
+        });
+    }
+
+    nextButton.addEventListener('click', () => {
+        if (currentCircle < circles.length) {
+            currentCircle++;
+            updateProgress();
+            updateButtons();
+        }
+    });
+
+    prevButton.addEventListener('click', () => {
+        if (currentCircle > 1) {
+            currentCircle--;
+            updateProgress();
+            updateButtons();
+        }
+    });
